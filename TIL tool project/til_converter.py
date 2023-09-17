0ba752c777aa63213975b6549d380fba22f1b100
alikhan1998 <77904698+alikhan1998@users.noreply.github.com>
2023-09-17 15:50:10 -0300

    Update til_converter.py

--- TIL tool project/til_converter.py
+++ TIL tool project/til_converter.py
@@ -1,13 +1,12 @@
 import os
 import argparse
-import markdown
+import markdown2
 
 def convert_to_html(markdown_text):
     # Convert Markdown to HTML
-    html_content = markdown.markdown(markdown_text)
-    return html_content
+    return markdown2.markdown(markdown_text)
 
-def process_file(input_file, output_folder, custom_css=None):
+def process_file(input_file, output_folder, stylesheet_url=None):
     # Read the content of the input file
     with open(input_file, 'r') as file:
         markdown_text = file.read()
@@ -19,46 +18,39 @@ def process_file(input_file, output_folder, custom_css=None):
     input_filename = os.path.basename(input_file)
     output_file = os.path.join(output_folder, input_filename.replace('.txt', '.html'))
 
-    # Create the HTML template with custom styles
-    html_template = f'''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>{input_filename}</title>
-        <link rel="stylesheet" href="{custom_css or 'default.css'}">
-    </head>
-    <body>
-        {html_content}
-    </body>
-    </html>
-    '''
-
     # Write the HTML content to the output file
     with open(output_file, 'w') as file:
-        file.write(html_template)
+        if stylesheet_url:
+            # Include the custom stylesheet URL in the HTML header
+            file.write(f'<!DOCTYPE html>\n<html>\n<head>\n'
+                       f'<link rel="stylesheet" href="{stylesheet_url}">\n'
+                       f'<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">\n'
+                       f'<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>\n'
+                       f'<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>\n'
+                       f'<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>\n'
+                       f'</head>\n<body>\n')
+        else:
+            file.write('<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\n')
+
+        # Write the HTML content
+        file.write(html_content)
+
+        # Close the HTML tags
+        file.write('\n</body>\n</html>')
 
 def main():
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description='TIL Tool - Convert Markdown to HTML')
-    parser.add_argument('input', help='Input .txt file or folder of .txt files')
+    parser.add_argument('input', help='Input .txt file')
     parser.add_argument('-o', '--output', default='til', help='Output directory for HTML files')
-    parser.add_argument('-s', '--stylesheet', help='Custom CSS stylesheet file')
+    parser.add_argument('-s', '--stylesheet', help='Custom stylesheet URL')
     args = parser.parse_args()
 
     # Create the output folder if it doesn't exist
     os.makedirs(args.output, exist_ok=True)
 
-    # Process the input (file or folder)
-    if os.path.isfile(args.input):
-        process_file(args.input, args.output, args.stylesheet)
-    elif os.path.isdir(args.input):
-        # Process all .txt files in the input folder
-        for filename in os.listdir(args.input):
-            if filename.endswith('.txt'):
-                input_file = os.path.join(args.input, filename)
-                process_file(input_file, args.output, args.stylesheet)
+    # Process the input file
+    process_file(args.input, args.output, args.stylesheet)
 
 if __name__ == '__main__':
     main()
