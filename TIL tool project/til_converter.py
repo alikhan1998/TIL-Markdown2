129099f8f9bd4a528306376a88ef6fedadf07b78
Katie Liu <kliu57@myseneca.ca>
2023-10-06 03:11:52 -0400

    support for config file implemented and tested

--- TIL tool project/til_converter.py
+++ TIL tool project/til_converter.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import sys
 import markdown2
 from tomlkit.toml_file import TOMLFile
 
@@ -57,18 +58,51 @@ def get_config_file_data(config_file):
         return None
 
 def main():
+    output_path = None
+    stylesheet_url = None
+
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description='TIL Tool - Convert Markdown to HTML')
     parser.add_argument('input', help='Input .txt file')
     parser.add_argument('-o', '--output', default='til', help='Output directory for HTML files')
     parser.add_argument('-s', '--stylesheet', help='Custom stylesheet URL')
+    parser.add_argument('-c', '--config', help='Use config file instead of command-line args')
     args = parser.parse_args()
 
+    # Check if user has opted to use a config file
+    if args.config:
+        # If config file does not exist, exit with error message
+        if not os.path.isfile(args.config):
+            print(f"Error: Config file {args.config} does not exist\n")
+            sys.exit()
+
+        # Extract data from config file into a dictionary-like object
+        config_args = get_config_file_data(args.config)
+
+        # If data extraction from config file failed, exit with error message
+        if not config_args:
+            print(f"Error: Unable to parse config file {args.config} as TOML\n")
+            sys.exit()
+        
+        # Get output path from config file
+        if "output" in config_args:
+            output_path = config_args["output"]
+
+        # Get stylesheet url from config file
+        if "stylesheet" in config_args:
+            stylesheet_url = config_args["stylesheet"]
+    else:
+        # Get output path from command-line args
+        output_path = args.output
+
+        # Get stylesheet url from command-line args
+        stylesheet_url = args.stylesheet 
+
     # Create the output folder if it doesn't exist
-    os.makedirs(args.output, exist_ok=True)
+    os.makedirs(output_path, exist_ok=True)
 
     # Process the input file
-    process_file(args.input, args.output, args.stylesheet)
+    process_file(args.input, output_path, stylesheet_url)
 
 if __name__ == '__main__':
     main()
